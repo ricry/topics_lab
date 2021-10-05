dbbcc26736dd33942eb407666245aa14f509a49d
平垣内唯織 <hiragauchiiori@gauchis-MacBook-Pro.local>
2021-10-05 13:48:18 +0900

    コメントにいいねボタンを追加

--- front/topicslab/src/components/Comments.vue
+++ front/topicslab/src/components/Comments.vue
@@ -4,8 +4,19 @@
       <template #legend>
         <span>{{comment.user.name}}</span>
       </template>
-      <div class="comment-text">
-        {{comment.body}}
+      <div class="comment-wrap">
+        <div class="comment-text">
+          {{comment.body}}
+        </div>
+        <div class="like-btn">
+          <span class="commentlike-text">{{ comment.likes_count }}</span>
+          <Button
+            icon="pi pi-heart"
+            class="comment-like p-button-rounded p-button-help"
+            v-on:click="isActive = !isActive"
+            v-bind:class="classColorSet"
+          />
+        </div>
       </div>
     </Fieldset>
   </div>
@@ -16,6 +27,19 @@ export default {
   name: 'Comments',
   props: {
     comments: Array
+  },
+  data () {
+    return {
+      isActive: true
+    }
+  },
+  computed: {
+    classColorSet: function () {
+      return {
+        before: this.isActive,
+        after: !this.isActive
+      }
+    }
   }
 }
 </script>
@@ -27,5 +51,15 @@ export default {
 
 .comment-text {
   white-space:pre-wrap;
+}
+.before {
+  background-color: lightgray;
+  border: none;
+}
+.after {
+  background-color: #c94297;
+}
+.comment-wrap {
+  display: flex;
 }
 </style>
