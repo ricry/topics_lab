5b54a613cf6cb52961edf30dc0d7ae56a468ca47
Hashimoto0226 <68983748+Hashimoto0226@users.noreply.github.com>
2021-10-05 14:30:13 +0900

    トップページに遷移

--- front/topicslab/src/views/Login.vue
+++ front/topicslab/src/views/Login.vue
@@ -48,6 +48,7 @@ export default {
             .then((res) => {
               if (res.status === 200) {
                 console.log('ログイン成功')
+                this.$router.push('/')
                 localStorage.setItem('authenticated', 'true')
               } else {
                 this.message = 'ログインに失敗しました。'
